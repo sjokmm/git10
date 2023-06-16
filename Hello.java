75fca7c4ffe60d4a73e8e9b43ae90cbe3f20c7fc
protocols <sjokmm@naver.com>
2023-06-16 12:18:40 +0900

    fifth commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
-		System.out.println("Hello Clone");
- 	}	
+	for(int i=0; i<10; i++){
+		System.out.println("hello world!");
+   }
+  }	
 }
